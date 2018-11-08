22c99e7218a1013a658d1919a5344881365491ec
AlexandreDejous <alex.dejous@gmail.com>
2018-11-08 15:53:21 +0100

    Before Fabcar test

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter); //TRIGGERS WHEN URL, ACTIVATE JS (in routes)
+app.use('/', indexRouter); //TRIGGERS WHEN URL, ACTIVATE NODE.JS (in routes)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
-  res.render('index', { title: 'Express' });
-
+  res.render('layout', { title: 2 });
+  
 });
 
 module.exports = router;
