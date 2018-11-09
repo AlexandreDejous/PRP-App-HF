16a1b4b2778899c135c87e0b6958e32b8bae5be8
AlexandreDejous <alex.dejous@gmail.com>
2018-11-09 12:13:40 +0100

    Click worked

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //The path to public
 
 app.use('/', indexRouter); //TRIGGERS WHEN URL, ACTIVATE JS (in routes)
 app.use('/users', usersRouter);
+
+//////////////////////////test area
+app.post('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+});
+/////////////////////////test area
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
